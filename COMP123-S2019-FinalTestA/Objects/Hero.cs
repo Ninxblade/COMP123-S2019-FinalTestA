67f135283a5d294eb9791e13f69563a9c75b092a
JasonLi <jasonli321@hotmail.ca>
2019-08-09 09:05:47 -0400

    Add project files.

---
+++ COMP123-S2019-FinalTestA/Objects/Hero.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 
+ * STUDNET NAME: Jason Li
+ * STUDENT ID: 301012468
+ * DESCRIPTION : This is the Hero Data Contrainer Class 
+ */
+namespace COMP123_S2019_FinalTestA.Objects
+{
+    class Hero
+    {
+        //Identity
+        public string HeroName { get; set; }
+        public string FristName { get; set; }
+        public string LastName { get; set; }
+
+        //Physical Abilities
+        public string Fighting { get; set; }
+        public string Strength { get; set; }
+        public string Agility { get; set; }
+        public string Endurance { get; set; }
+
+        // Mental Abilities
+        public string Reason { get; set; }
+        public string Intuition { get; set; }
+        public string Psyche { get; set; }
+        public string Popularity { get; set; }
+        public string Name { get; set; }
+
+        //Power List
+        List<Power> Powers;
+
+        //Consutructor 
+        Hero()
+        {
+            Powers = new List<Power>();
+        }
+
+    }
+}
---
+++ COMP123-S2019-FinalTestA/Objects/Power.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 
+ * STUDNET NAME: Jason Li
+ * STUDENT ID: 301012468
+ * DESCRIPTION : This is the Power Class 
+ */
+namespace COMP123_S2019_FinalTestA.Objects
+{
+    class Power
+    {
+        //Public Properties
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+}
